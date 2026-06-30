1242a72d0d51e04d4b405c530c8947e4c6b426d1
Ankush Tiwari <mr.ankushji.708@gmail.com>
2026-06-30 17:20:46 +0530

    Added solution for Roman to Integer (LeetCode 13)
    Solved by converting Roman numerals into their integer values and then traversing the array to apply the subtraction rule when a smaller numeral appears before a larger one.
    
    Time Complexity: O(n)
    Space Complexity: O(n)
    
    Note: This solution uses an auxiliary array. It can be further optimized to O(1) extra space by processing the string directly without storing intermediate values.

---
+++ Strings/RomanToInteger.java
@@ -0,0 +1,47 @@
+/*
+Problem: Roman to Integer
+LeetCode: 13
+Difficulty: Easy
+Topic: Strings
+
+Problem Link:
+https://leetcode.com/problems/roman-to-integer/
+
+Date Solved:
+21-06-2026
+*/
+
+class Solution {
+    public int romanToInt(String s) {
+
+        int i = 0;
+        int[] arr = new int[s.length()];
+
+        for(char ch : s.toCharArray()) {
+
+            switch(ch) {
+                case 'I': arr[i] = 1; break;
+                case 'V': arr[i] = 5; break;
+                case 'X': arr[i] = 10; break;
+                case 'L': arr[i] = 50; break;
+                case 'C': arr[i] = 100; break;
+                case 'D': arr[i] = 500; break;
+                case 'M': arr[i] = 1000; break;
+            }
+
+            i++;
+        }
+
+        int result = 0;
+
+        for(int j = 0; j < arr.length; j++) {
+
+            if(j < arr.length - 1 && arr[j] < arr[j + 1])
+                result -= arr[j];
+            else
+                result += arr[j];
+        }
+
+        return result;
+    }
+}
